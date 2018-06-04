e2a04c70d59acf0af676d82f12ab69149382b10a
Jiong Zhu <zhxyimm@sina.cn>
2018-06-04 20:47:37 +0800

    init project

---
+++ src/main/java/com/jiong/encounter/EncounterApplication.java
@@ -0,0 +1,12 @@
+package com.jiong.encounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncounterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jiong/encounter/EncounterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jiong.encounter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EncounterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
